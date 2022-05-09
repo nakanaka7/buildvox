97991fc564366a9dc26d1c8e4a616dfc1ef20967
nakanaka7 <nakanaka7a@gmail.com>
2022-05-09 10:11:44 +0900

    copied code of PosMarkerClickBlockEventHandler.onLeft() and onRight()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/system/BuildVoxSystem.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/system/BuildVoxSystem.java
@@ -7,6 +7,8 @@ import tokyo.nakanaka.buildvox.core.*;
 import tokyo.nakanaka.buildvox.core.blockStateTransformer.BlockStateTransformer;
 import tokyo.nakanaka.buildvox.core.command.bvCommand.BvCommand;
 import tokyo.nakanaka.buildvox.core.command.bvdCommand.BvdCommand;
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
+import tokyo.nakanaka.buildvox.core.player.Player;
 import tokyo.nakanaka.buildvox.core.system.clickBlockEventHandler.PosMarkerClickBlockEventHandler;
 import tokyo.nakanaka.buildvox.core.world.Block;
 import tokyo.nakanaka.buildvox.core.world.World;
@@ -124,14 +126,66 @@ public class BuildVoxSystem {
         return Util.getTabCompletionList(spec, args);
     }
 
-    /** Handles a left-clicking block event by pos marker. */
+    /**
+     * Handles a left-clicking block event by pos marker.
+     * @param playerId the id of a player who invoked this event.
+     * @param worldId the world id of the clicked block.
+     * @param x the x-coordinate of the clicked block.
+     * @param y the y-coordinate of the clicked block.
+     * @param z the z-coordinate of the clicked block.
+     * @param messageReceiver the receiver of the feedback message of this event.
+     * @throws IllegalArgumentException if the player id is not registered into this class.
+     * @throws IllegalArgumentException if the world id is not registered.
+     */
     public static void onLeftClickBlockByPosMarker(UUID playerId, NamespacedId worldId, int x, int y, int z, MessageReceiver messageReceiver) {
-        new PosMarkerClickBlockEventHandler().onLeft(playerId, worldId, x, y, z, messageReceiver);
+        if(!BuildVoxSystem.WORLD_REGISTRY.worldIsRegistered(worldId)) {
+            throw new IllegalArgumentException();
+        }
+        World world = BuildVoxSystem.WORLD_REGISTRY.get(worldId);
+        Player player = BuildVoxSystem.PLAYER_REPOSITORY.get(playerId);
+        if(player == null)throw new IllegalArgumentException();
+        Vector3i[] posData = new Vector3i[player.getPosArrayClone().length];
+        posData[0] = new Vector3i(x, y, z);
+        player.setPosArrayWithSelectionNull(world, posData);
+        PARTICLE_GUI_REPOSITORY.update(player);
+        messageReceiver.println(config.outColor() + FeedbackMessage.ofPosExit(0, x, y, z));
     }
 
-    /** Handles a right-clicking block event by pos marker. */
+    /**
+     * Handles a right-clicking block event by pos marker.
+     * @param playerId the id of a player who invoked this event.
+     * @param worldId the world id of the clicked block.
+     * @param x the x-coordinate of the clicked block.
+     * @param y the y-coordinate of the clicked block.
+     * @param z the z-coordinate of the clicked block.
+     * @param messageReceiver the receiver of the feedback message of this event.
+     * @throws IllegalArgumentException if the player id is not registered into this class.
+     * @throws IllegalArgumentException if the world id is not registered.
+     */
     public static void onRightClickBlockByPosMarker(UUID playerId, NamespacedId worldId, int x, int y, int z, MessageReceiver messageReceiver) {
-        new PosMarkerClickBlockEventHandler().onRight(playerId, worldId, x, y, z, messageReceiver);
+        if(!BuildVoxSystem.WORLD_REGISTRY.worldIsRegistered(worldId)) {
+            throw new IllegalArgumentException();
+        }
+        World world = BuildVoxSystem.WORLD_REGISTRY.get(worldId);
+        Player player = BuildVoxSystem.PLAYER_REPOSITORY.get(playerId);
+        if(player == null)throw new IllegalArgumentException();
+        World posOrSelectionWorld = player.getWorld();
+        Vector3i[] posData = player.getPosArrayClone();
+        if(world != posOrSelectionWorld){
+            posData = new Vector3i[player.getPosArrayClone().length];
+        }
+        int dataSize = posData.length;
+        int index = dataSize - 1;
+        for(int i = 0; i < dataSize; ++i){
+            if (posData[i] == null){
+                index = i;
+                break;
+            }
+        }
+        posData[index] = new Vector3i(x, y, z);
+        player.setPosArrayWithSelectionNull(world, posData);
+        PARTICLE_GUI_REPOSITORY.update(player);
+        messageReceiver.println(config.outColor() + FeedbackMessage.ofPosExit(index, x, y, z));
     }
 
 }
