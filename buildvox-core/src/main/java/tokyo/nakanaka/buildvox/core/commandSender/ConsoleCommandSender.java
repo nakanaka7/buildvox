4b88993a774b125d961ae1f4a4221dc74241be33
nakanaka7 <nakanaka7a@gmail.com>
2022-05-09 20:21:49 +0900

    initial commit

---
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/ConsoleCommandSender.java
@@ -0,0 +1,18 @@
+package tokyo.nakanaka.buildvox.core.commandSender;
+
+import tokyo.nakanaka.buildvox.core.system.BuildVoxSystem;
+
+public abstract class ConsoleCommandSender implements CommandSender {
+    public abstract void sendMessage(String msg);
+
+    @Override
+    public void sendOutMessage(String msg) {
+        sendErrMessage(BuildVoxSystem.getConfig().outColor() + msg);
+    }
+
+    @Override
+    public void sendErrMessage(String msg) {
+        sendOutMessage(BuildVoxSystem.getConfig().errColor() + msg);
+    }
+
+}
