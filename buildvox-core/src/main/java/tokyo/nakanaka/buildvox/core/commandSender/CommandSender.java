7ab555a172825ce55722160a35c7dfe7e0c67d77
nakanaka7 <nakanaka7a@gmail.com>
2022-05-10 22:11:36 +0900

    add getWorld() and getBlockPos()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/CommandSender.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/CommandSender.java
@@ -1,6 +1,15 @@
 package tokyo.nakanaka.buildvox.core.commandSender;
 
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
+import tokyo.nakanaka.buildvox.core.world.World;
+
 public interface CommandSender {
     void sendOutMessage(String msg);
     void sendErrMessage(String msg);
+    default World getWorld() {
+        return null;
+    }
+    default Vector3i getBlockPos() {
+        return new Vector3i(0, 0, 0);
+    }
 }
