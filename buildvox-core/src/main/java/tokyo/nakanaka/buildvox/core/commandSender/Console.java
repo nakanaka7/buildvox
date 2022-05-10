7d9dae2bdb4e2e850f6fdffa424f08db0702368c
nakanaka7 <nakanaka7a@gmail.com>
2022-05-10 22:44:26 +0900

    add getWorld(), setWorld(), getBlockPos(), and setBlockPos()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/Console.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/Console.java
@@ -1,8 +1,13 @@
 package tokyo.nakanaka.buildvox.core.commandSender;
 
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.system.BuildVoxSystem;
+import tokyo.nakanaka.buildvox.core.world.World;
 
 public abstract class Console implements CommandSender {
+    private World world;
+    private Vector3i blockPos;
+
     public abstract void sendMessage(String msg);
 
     @Override
@@ -13,6 +18,24 @@ public abstract class Console implements CommandSender {
     @Override
     public void sendErrMessage(String msg) {
         sendOutMessage(BuildVoxSystem.getConfig().errColor() + msg);
+    }
+
+    @Override
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
+
+    @Override
+    public Vector3i getBlockPos() {
+        return blockPos;
+    }
+
+    public void setBlockPos(Vector3i blockPos) {
+        this.blockPos = blockPos;
     }
 
 }
