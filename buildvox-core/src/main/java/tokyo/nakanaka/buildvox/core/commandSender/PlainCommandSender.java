0b86d7d37eb91295c5d79030f0db21136c146595
nakanaka7 <nakanaka7a@gmail.com>
2022-05-11 15:44:43 +0900

    initial commit

---
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/PlainCommandSender.java
@@ -0,0 +1,37 @@
+package tokyo.nakanaka.buildvox.core.commandSender;
+
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
+import tokyo.nakanaka.buildvox.core.world.World;
+
+public abstract class PlainCommandSender implements CommandSender {
+    private World world;
+    private Vector3i blockPos;
+
+    public PlainCommandSender(World world, Vector3i blockPos) {
+        this.world = world;
+        this.blockPos = blockPos;
+    }
+
+    public abstract void sendMessage(String  msg);
+
+    @Override
+    public void sendOutMessage(String msg) {
+        sendMessage(msg);
+    }
+
+    @Override
+    public void sendErrMessage(String msg) {
+        sendMessage(msg);
+    }
+
+    @Override
+    public World getWorld() {
+        return world;
+    }
+
+    @Override
+    public Vector3i getBlockPos() {
+        return blockPos;
+    }
+
+}
