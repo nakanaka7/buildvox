342e2f5d116811ceedf698ad477a48d096e8c022
nakanaka7 <nakanaka7a@gmail.com>
2022-05-11 10:39:11 +0900

    removed default modifier from getWorld() and getBlockPos()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/CommandSender.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/CommandSender.java
@@ -6,10 +6,6 @@ import tokyo.nakanaka.buildvox.core.world.World;
 public interface CommandSender {
     void sendOutMessage(String msg);
     void sendErrMessage(String msg);
-    default World getWorld() {
-        return null;
-    }
-    default Vector3i getBlockPos() {
-        return new Vector3i(0, 0, 0);
-    }
+    World getWorld();
+    Vector3i getBlockPos();
 }
