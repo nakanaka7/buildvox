6bb5042d3de402806f8f0ab9c5f7328d6298ae5d
nakanaka7 <nakanaka7a@gmail.com>
2022-05-12 11:03:11 +0900

    deleted unused methods

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/system/WorldRegistry.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/system/WorldRegistry.java
@@ -5,14 +5,9 @@ import tokyo.nakanaka.buildvox.core.world.World;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class WorldRegistry {
     private final Map<NamespacedId, World> worldMap = new HashMap<>();
-
-    public void register(NamespacedId id, World world) {
-        worldMap.put(id, world);
-    }
 
     public void register(World world) {
         worldMap.put(world.getId(), world);
@@ -22,21 +17,8 @@ public class WorldRegistry {
         worldMap.remove(id);
     }
 
-    public boolean worldIsRegistered(NamespacedId id) {
-        return worldMap.containsKey(id);
-    }
-
     public World get(NamespacedId id) {
         return worldMap.get(id);
-    }
-
-    public Set<NamespacedId> worldIdSet() {
-        return worldMap.keySet();
-    }
-
-    @Deprecated
-    public Map<NamespacedId, World> getWorldMap() {
-        return worldMap;
     }
 
 }
