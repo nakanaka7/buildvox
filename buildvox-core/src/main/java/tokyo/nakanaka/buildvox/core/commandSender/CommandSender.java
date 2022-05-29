04b9cacc5d793b7df45446b803630cd6563449f3
nakanaka7 <nakanaka7a@gmail.com>
2022-05-29 23:49:10 +0900

    wrote javadoc

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/CommandSender.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/CommandSender.java
@@ -3,9 +3,31 @@ package tokyo.nakanaka.buildvox.core.commandSender;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.world.World;
 
+/**
+ * Represents a command sender.
+ */
 public interface CommandSender {
+    /**
+     * Send a (non-error) message.
+     * @param msg the message.
+     */
     void sendOutMessage(String msg);
+
+    /**
+     * Send an error message.
+     * @param msg the message.
+     */
     void sendErrMessage(String msg);
+
+    /**
+     * Get the world of the command sender.
+     * @return the world
+     */
     World getWorld();
+
+    /**
+     * Get the block position of the command sender.
+     * @return the block position
+     */
     Vector3i getBlockPos();
 }
