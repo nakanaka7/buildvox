a79431ef74019117867269a367c418353051135f
nakanaka7 <nakanaka7a@gmail.com>
2022-05-30 00:24:36 +0900

    wrote javadoc

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/AbstractConsole.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/commandSender/AbstractConsole.java
@@ -4,10 +4,34 @@ import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.system.BuildVoxSystem;
 import tokyo.nakanaka.buildvox.core.world.World;
 
+/**
+ * Represents a console command sender. This command sender has its world and block position.  The initial block
+ * position will be (0, 0, 0).
+ */
 public abstract class AbstractConsole implements CommandSender {
     private World world;
     private Vector3i blockPos;
 
+    /**
+     * The initial world should be given.
+     */
+    @Deprecated
+    public AbstractConsole() {
+    }
+
+    /**
+     * Constructs a console command sender.
+     * @param world the initial world of the console command sender.
+     */
+    public AbstractConsole(World world) {
+        this.world = world;
+        this.blockPos = Vector3i.ZERO;
+    }
+
+    /**
+     * Send the message without any modification.
+     * @param msg a message.
+     */
     public abstract void sendMessage(String msg);
 
     @Override
@@ -25,6 +49,10 @@ public abstract class AbstractConsole implements CommandSender {
         return world;
     }
 
+    /**
+     * Set the world.
+     * @param world a new world
+     */
     public void setWorld(World world) {
         this.world = world;
     }
@@ -34,6 +62,10 @@ public abstract class AbstractConsole implements CommandSender {
         return blockPos;
     }
 
+    /**
+     * Set the block position.
+     * @param blockPos a new block position
+     */
     public void setBlockPos(Vector3i blockPos) {
         this.blockPos = blockPos;
     }
