c1a8a414505a041dae8024939e0b281da5a23f8f
nakanaka7 <nakanaka7a@gmail.com>
2022-05-30 17:04:46 +0900

    refactored setParticleGuiVisible() and updateParticleGui()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/player/Player.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/player/Player.java
@@ -2,10 +2,13 @@ package tokyo.nakanaka.buildvox.core.player;
 
 import tokyo.nakanaka.buildvox.core.commandSender.CommandSender;
 import tokyo.nakanaka.buildvox.core.edit.Clipboard;
+import tokyo.nakanaka.buildvox.core.math.region3d.Parallelepiped;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3d;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.particleGui.Color;
-import tokyo.nakanaka.buildvox.core.particleGui.ParticleGuiRepository;
+import tokyo.nakanaka.buildvox.core.particleGui.ParticleGui;
+import tokyo.nakanaka.buildvox.core.selection.FillSelection;
+import tokyo.nakanaka.buildvox.core.selection.PasteSelection;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
 import tokyo.nakanaka.buildvox.core.system.BuildVoxSystem;
 import tokyo.nakanaka.buildvox.core.system.Entity;
@@ -30,6 +33,7 @@ public class Player implements CommandSender, Entity<UUID> {
     private Selection selection;
     private PlayerEntity playerEntity;
     private boolean particleGuiVisible;
+    private ParticleGui particleGui;
 
     public Player(PlayerEntity playerEntity) {
         this.id = playerEntity.getId();
@@ -219,17 +223,75 @@ public class Player implements CommandSender, Entity<UUID> {
      */
     public void setParticleGuiVisible(boolean b) {
         this.particleGuiVisible = b;
-        if(b) {
-            ParticleGuiRepository.PARTICLE_GUI_REPOSITORY.create(this);
+        if (b) {
+            if(playerEntity == null) return;
+            particleGui = new ParticleGui(BuildVoxSystem.environment.scheduler());
+            particleGui.setOut(playerEntity);
             updateParticleGui();
-        }else{
-            ParticleGuiRepository.PARTICLE_GUI_REPOSITORY.delete(this);
+        } else {
+            particleGui.close();
         }
     }
 
     private void updateParticleGui() {
         if(!particleGuiVisible) return;
-        ParticleGuiRepository.PARTICLE_GUI_REPOSITORY.update(this);
+        if(particleGui == null)return;
+        particleGui.clearAllLines();
+        World posOrSelectionWorld = getEditTargetWorld();
+        if(posOrSelectionWorld == null)return;
+        Selection selection = getSelection();
+        if(selection != null){
+            Color color;
+            if(selection instanceof PasteSelection){
+                color = Color.YELLOW;
+            }else if(selection instanceof FillSelection){
+                color = Color.LIME;
+            }else{
+                color = Color.MAGENTA;
+            }
+            particleGui.addParallelepipedLines(color, posOrSelectionWorld, selection.getBound());
+        }
+        Vector3i[] posData = getPosArrayClone();
+        for(int i = 0; i < posData.length; ++ i){
+            Vector3i pos = posData[i];
+            if(pos != null){
+                Color color;
+                if(i == 0){
+                    color = Color.RED;
+                }else if(i == 1){
+                    color = Color.BLUE;
+                }else if(i == 2){
+                    color = Color.YELLOW;
+                }else if(i == 3){
+                    color = Color.LIME;
+                }else{
+                    throw new InternalError();
+                }
+                particleGui.addBlockLines(color, posOrSelectionWorld, (int)Math.floor(pos.x()), (int)Math.floor(pos.y()), (int)Math.floor(pos.z()));
+                for(int j = i + 1; j < posData.length; ++ j){
+                    Vector3i posJ = posData[j];
+                    if(posJ != null){
+                        particleGui.addLine(Color.CYAN, posOrSelectionWorld,
+                                pos.x() + 0.5, pos.y() + 0.5, pos.z() + 0.5,
+                                posJ.x() + 0.5, posJ.y() + 0.5, posJ.z() + 0.5);
+                    }
+                }
+            }
+        }
+        if(posData.length == 2){
+            Vector3i pos0 = posData[0];
+            Vector3i pos1 = posData[1];
+            if(pos0 != null && pos1 != null){
+                double px = Math.max(pos0.x(), pos1.x()) + 1;
+                double py = Math.max(pos0.y(), pos1.y()) + 1;
+                double pz = Math.max(pos0.z(), pos1.z()) + 1;
+                double nx = Math.min(pos0.x(), pos1.x());
+                double ny = Math.min(pos0.y(), pos1.y());
+                double nz = Math.min(pos0.z(), pos1.z());
+                Parallelepiped parallelepiped = new Parallelepiped(px, py, pz, nx, ny, nz);
+                particleGui.addParallelepipedLines(Color.CYAN, posOrSelectionWorld, parallelepiped);
+            }
+        }
     }
 
 }
