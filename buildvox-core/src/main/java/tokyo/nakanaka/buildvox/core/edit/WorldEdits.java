93e7a12199be27af4728b0e8f9db25b19914e55e
nakanaka7 <nakanaka7a@gmail.com>
2022-06-02 20:54:48 +0900

    wrote javadoc

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/WorldEdits.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/WorldEdits.java
@@ -19,57 +19,108 @@ import tokyo.nakanaka.buildvox.core.blockSpace.BlockSpace3;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * The utility class of world edits.
+ */
 public class WorldEdits {
     private WorldEdits() {
     }
 
     /**
-     * Copies the blocks in selection. A coordinate system is made which origin is the offset.
+     * Copies the blocks in selection. A coordinate system is made which origin is the pos.
      * Each coordinate axis is divided by length 1, and the space is full by 1x1x1 cubes as a result. If the center of
      * the cube is contained in the selection, the cube is copy target. The copy block type is
      * the type of the center, and the block position in the clipboard is the same position as the made coordinate
      * system(Not original world coordinate system).
+     * @param srcWorld the source world.
+     * @param srcSel the source selection.
+     * @param pos the position of the source which corresponds to the origin of the clipboard.
+     * @param dest the destination clipboard.
      */
-    public static void copy(EditWorld editWorld, Selection selection, Vector3d offset, Clipboard clipboard) {
-        var dest = new ClipboardBlockSpace3(clipboard);
-        var trans = AffineTransformation3d.ofTranslation(-offset.x(), -offset.y(), -offset.z());
-        BlockSpaceEdits.copy(editWorld, selection.calculateBlockPosSet(), dest, trans);
+    public static void copy(EditWorld srcWorld, Selection srcSel, Vector3d pos, Clipboard dest) {
+        var clipDest = new ClipboardBlockSpace3(dest);
+        var trans = AffineTransformation3d.ofTranslation(-pos.x(), -pos.y(), -pos.z());
+        BlockSpaceEdits.copy(srcWorld, srcSel.calculateBlockPosSet(), clipDest, trans);
     }
 
+    /**
+     * Copies the blocks of the source selection into the destination clipboard. The pos of the selection corresponds to
+     * the origin of the clipboard.
+     * @param srcWorld the source world.
+     * @param srcSel the source selection.
+     * @param pos the position of the source which corresponds to the origin of the clipboard.
+     * @param dest the destination clipboard.
+     */
     public static void copy(World srcWorld, Selection srcSel, Vector3d pos, Clipboard dest) {
         var destBs = new ClipboardBlockSpace3(dest);
         var trans = AffineTransformation3d.ofTranslation(-pos.x(), -pos.y(), -pos.z());
         BlockSpaceEdits.copy(new EditWorld(srcWorld), srcSel.calculateBlockPosSet(), destBs, trans);
     }
 
-    public static void paste(Clipboard clipboard, EditWorld editWorld, Vector3d offset) {
-        paste(clipboard, editWorld, offset, AffineTransformation3d.IDENTITY);
+    /**
+     * Pastes the clipboard contents into the destination world.
+     * @param srcClip the source clipboard.
+     * @param dest the destination world.
+     * @param pos the position of the world which corresponds to the origin of the clipboard.
+     */
+    public static void paste(Clipboard srcClip, EditWorld dest, Vector3d pos) {
+        paste(srcClip, dest, pos, AffineTransformation3d.IDENTITY);
     }
 
-    public static void paste(Clipboard clipboard, EditWorld editWorld, Vector3d offset, AffineTransformation3d clipboardTrans) {
-        paste(clipboard, editWorld, offset, clipboardTrans, 1);
+    /**
+     * Pastes the clipboard contents into the destination world.
+     * @param srcClip the source clipboard.
+     * @param dest the destination world.
+     * @param pos the position of the world which corresponds to the origin of the clipboard.
+     * @param clipboardTrans the clipboard transformation
+     */
+    public static void paste(Clipboard srcClip, EditWorld dest, Vector3d pos, AffineTransformation3d clipboardTrans) {
+        paste(srcClip, dest, pos, clipboardTrans, 1);
     }
 
-    public static void paste(Clipboard clipboard, EditWorld editWorld, Vector3d offset, AffineTransformation3d clipboardTrans, double integrity) {
-        BlockSpace3<Block> src = new ClipboardBlockSpace3(clipboard);
-        Set<Vector3i> srcPosSet = clipboard.blockPosSet();
+    /**
+     * Pastes the clipboard contents into the destination world.
+     * @param srcClip the source clipboard.
+     * @param dest the destination world.
+     * @param pos the position of the world which corresponds to the origin of the clipboard.
+     * @param clipboardTrans the clipboard transformation
+     * @param integrity the integrity of the block-settings.
+     */
+    public static void paste(Clipboard srcClip, EditWorld dest, Vector3d pos, AffineTransformation3d clipboardTrans, double integrity) {
+        BlockSpace3<Block> src = new ClipboardBlockSpace3(srcClip);
+        Set<Vector3i> srcPosSet = srcClip.blockPosSet();
         BlockTransformation blockTrans = BlockTransformApproximator.approximateToBlockTrans(clipboardTrans);
-        BlockSpace3<Block> dest = new BlockStateTransformingBlockSpace3(editWorld, BuildVoxSystem.environment.blockStateTransformer(), blockTrans);
-        dest = new IntegrityAdjustableBlockSpace3<>(dest, integrity);
-        AffineTransformation3d trans = AffineTransformation3d.ofTranslation(offset.x(), offset.y(), offset.z()).compose(clipboardTrans);
-        BlockSpaceEdits.copy(src, srcPosSet, dest, trans);
+        BlockSpace3<Block> transDest = new BlockStateTransformingBlockSpace3(dest, BuildVoxSystem.environment.blockStateTransformer(), blockTrans);
+        transDest = new IntegrityAdjustableBlockSpace3<>(transDest, integrity);
+        AffineTransformation3d trans = AffineTransformation3d.ofTranslation(pos.x(), pos.y(), pos.z()).compose(clipboardTrans);
+        BlockSpaceEdits.copy(src, srcPosSet, transDest, trans);
     }
 
-    public static void fill(EditWorld editWorld, Selection selection, Block block, double integrity) {
-        BlockSpace3<Block> dest = new IntegrityAdjustableBlockSpace3<>(editWorld, integrity);
-        BlockSpaceEdits.fill(dest, selection.calculateBlockPosSet(), block);
+    /**
+     * Fill the block in the selection.
+     * @param world the world.
+     * @param sel the selection.
+     * @param block the block.
+     * @param integrity the integrity of block-setting.
+     */
+    public static void fill(EditWorld world, Selection sel, Block block, double integrity) {
+        BlockSpace3<Block> dest = new IntegrityAdjustableBlockSpace3<>(world, integrity);
+        BlockSpaceEdits.fill(dest, sel.calculateBlockPosSet(), block);
     }
 
-    public static void replace(EditWorld editWorld, Selection selection, Block fromBlock, Block toBlock, double integrity) {
-        BlockSpace3<Block> space = new IntegrityAdjustableBlockSpace3<>(editWorld, integrity);
+    /**
+     * Replaces the blocks in the selection.
+     * @param world the world
+     * @param sel the selection
+     * @param fromBlock the block type to be replaced from.
+     * @param toBlock the block type to be replaced to.
+     * @param integrity the integrity of block-setting.
+     */
+    public static void replace(EditWorld world, Selection sel, Block fromBlock, Block toBlock, double integrity) {
+        BlockSpace3<Block> space = new IntegrityAdjustableBlockSpace3<>(world, integrity);
         NamespacedId fromId = fromBlock.getId();
         Map<String, String> fromStateMap = fromBlock.getStateMap();
-        BlockSpaceEdits.replace(space, selection.calculateBlockPosSet(),
+        BlockSpaceEdits.replace(space, sel.calculateBlockPosSet(),
             (BlockSpaceEdits.BlockCondition<Block>) (block) -> {
                 NamespacedId id = block.getId();
                 Map<String, String> stateMap = block.getStateMap();
@@ -83,7 +134,16 @@ public class WorldEdits {
             toBlock);
     }
 
-    public static void repeat(EditWorld editWorld, Vector3i pos0, Vector3i pos1,
+    /**
+     * Repeats the blocks in the cuboid specified by pos0 and pos1.
+     * @param world the world
+     * @param pos0 the position of a corner.
+     * @param pos1 the position of another corner.
+     * @param countX the count along x-axis.
+     * @param countY the count along y-axis.
+     * @param countZ the count along z-axis.
+     */
+    public static void repeat(EditWorld world, Vector3i pos0, Vector3i pos1,
                               int countX, int countY, int countZ) {
         int maxX = Math.max(pos0.x(), pos1.x());
         int maxY = Math.max(pos0.y(), pos1.y());
@@ -94,7 +154,7 @@ public class WorldEdits {
         Cuboid cuboid = new Cuboid(maxX + 1, maxY + 1, maxZ + 1, minX, minY, minZ);
         Selection selection = new Selection(cuboid, cuboid);
         Clipboard clipboard = new Clipboard();
-        WorldEdits.copy(editWorld, selection, new Vector3d(minX, minY, minZ), clipboard);
+        WorldEdits.copy(world, selection, new Vector3d(minX, minY, minZ), clipboard);
         clipboard.lock();
         for(int y = Math.min(0, countY); y <= Math.max(0, countY); ++y){
             for(int x = Math.min(0, countX); x <= Math.max(0, countX); ++x){
@@ -102,7 +162,7 @@ public class WorldEdits {
                     int offsetX = minX + x * (maxX - minX + 1);
                     int offsetY = minY + y * (maxY - minY + 1);
                     int offsetZ = minZ + z * (maxZ - minZ + 1);
-                    paste(clipboard, editWorld, new Vector3d(offsetX, offsetY, offsetZ));
+                    paste(clipboard, world, new Vector3d(offsetX, offsetY, offsetZ));
                 }
             }
         }
