b3374f14fe4e4330629d5140d331375f7ae38760
nakanaka7 <nakanaka7a@gmail.com>
2022-06-06 19:58:11 +0900

    add parseState() and parseEntity()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/block/Block.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/block/Block.java
@@ -29,4 +29,24 @@ public interface Block<S extends Block.State, E extends Block.Entity> extends En
      */
     S transformState(S state, BlockTransformation trans);
 
+    /**
+     * Parses the String to a Block.State. It is the inner part of [...].
+     * @param s a String.
+     * @return the State.
+     * @throws IllegalArgumentException if it cannot parse s.
+     */
+    default S parseState(String s) {
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * Parses the String to a Block.Entity. It is the inner part of {...}.
+     * @param s a String.
+     * @return the Entity.
+     * @throws IllegalArgumentException if it cannot parse s.
+     */
+    default E parseEntity(String s) {
+        throw new IllegalArgumentException();
+    }
+
 }
