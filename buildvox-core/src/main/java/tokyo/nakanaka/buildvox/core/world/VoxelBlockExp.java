ad5aa9aeeb33794d6f6c49ef449e414d091dbd54
nakanaka7 <nakanaka7a@gmail.com>
2022-06-06 17:47:33 +0900

    add Entity part

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/world/VoxelBlockExp.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/world/VoxelBlockExp.java
@@ -4,29 +4,42 @@ import tokyo.nakanaka.buildvox.core.blockStateTransformer.BlockTransformation;
 
 /**
  * Experimental.
- * Represents a block state.
+ * Represents a voxel block.
  * @param <S> the state object.
+ * @param <E> the entity object.
  */
-public class VoxelBlockExp<S> {
+public class VoxelBlockExp<S, E> {
     private final Block<S> block;
     private final S state;
+    private final E entity;
 
     /**
-     * Creates a block state.
+     * Creates a voxel block.
      * @param block the block.
      * @param state the state.
      */
     public VoxelBlockExp(Block<S> block, S state) {
-        this.block = block;
-        this.state = state;
+        this(block, state, null);
     }
 
     /**
-     * Transforms this block state.
-     * @param trans the block transformation.
-     * @return the transformed block state. The block will remain.
+     * Creates a voxel block.
+     * @param block the block.
+     * @param state the state.
+     * @param entity the entity.
      */
-    public VoxelBlockExp<S> transform(BlockTransformation trans) {
+    public VoxelBlockExp(Block<S> block, S state, E entity) {
+        this.block = block;
+        this.state = state;
+        this.entity = entity;
+    }
+
+    /**
+     * Transforms this voxel block.
+     * @param trans the block transformation.
+     * @return the transformed voxel block. The block will remain.
+     */
+    public VoxelBlockExp<S, E> transform(BlockTransformation trans) {
         S newState = block.transform(state, trans);
         return new VoxelBlockExp<>(block, newState);
     }
@@ -45,6 +58,14 @@ public class VoxelBlockExp<S> {
      */
     public S getState() {
         return state;
+    }
+
+    /**
+     * Gets the entity.
+     * @return the entity.
+     */
+    public E getEntity() {
+        return entity;
     }
 
 }
