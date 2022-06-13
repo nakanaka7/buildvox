98333358b536422ac0a973994be938b6086499d9
nakanaka7 <nakanaka7a@gmail.com>
2022-06-13 16:55:06 +0900

    refactored getBlock() and setBlock()

--- buildvox-bukkit/src/main/java/tokyo/nakanaka/buildvox/bukkit/BukkitWorld.java
+++ buildvox-bukkit/src/main/java/tokyo/nakanaka/buildvox/bukkit/BukkitWorld.java
@@ -10,7 +10,6 @@ import tokyo.nakanaka.buildvox.core.block.EntityImpl;
 import tokyo.nakanaka.buildvox.core.world.VoxelBlock;
 import tokyo.nakanaka.buildvox.core.world.World;
 
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
@@ -47,16 +46,11 @@ public class BukkitWorld implements World {
         return new NamespacedId(original.getName());
     }
 
-    @Override
-    public VoxelBlock getBlock(int x, int y, int z){
-        org.bukkit.block.Block voxel = original.getBlockAt(x, y, z);
-        return BukkitVoxelBlock.newInstance(voxel.getState());
-    }
-
     private static record BlockEntityContent(Set<BukkitVoxelBlock.BlockEntityData> blockEntityDataSet, Inventory inventory) {
     }
 
-    public VoxelBlock getBlockNew(int x, int y, int z) {
+    @Override
+    public VoxelBlock getBlock(int x, int y, int z) {
         org.bukkit.block.Block voxel = original.getBlockAt(x, y, z);
         var b = BukkitVoxelBlock.newInstance(voxel.getState());
         var block = b.getBlock();
@@ -71,32 +65,24 @@ public class BukkitWorld implements World {
     @Override
     public void setBlock(int x, int y, int z, VoxelBlock block, boolean physics){
         org.bukkit.block.Block targetBlock = original.getBlockAt(x, y, z);
-        if(block instanceof BukkitVoxelBlock bukkitBlock) {
-            bukkitBlock.setToWorld(server, targetBlock, physics);
-        }else{
-            String blockStr = block.toString();
-            BlockData blockData = server.createBlockData(blockStr);
-            targetBlock.setBlockData(blockData, physics);
-            targetBlock.getState().update();
-        }
-    }
-
-    public void setBlockNew(int x, int y, int z, VoxelBlock block, boolean physics){
-        org.bukkit.block.Block targetBlock = original.getBlockAt(x, y, z);
         String blockStr = block.toString();
         BlockData blockData = server.createBlockData(blockStr);
         targetBlock.setBlockData(blockData, physics);
         org.bukkit.block.BlockState blockState = targetBlock.getState();
         EntityImpl entity = (EntityImpl) block.getEntity();
-        BlockEntityContent entityContent = (BlockEntityContent) entity.getObj();
-        for(var blockEntityData : entityContent.blockEntityDataSet()) {
-            blockEntityData.merge(blockState);
-        }
-        blockState.update();
-        var inventory = entityContent.inventory();
-        if(blockState instanceof Container container && inventory != null) {
-            ItemStack[] contents = inventory.getContents();
-            container.getInventory().setContents(contents);
+        if(entity != null) {
+            BlockEntityContent entityContent = (BlockEntityContent) entity.getObj();
+            for (var blockEntityData : entityContent.blockEntityDataSet()) {
+                blockEntityData.merge(blockState);
+            }
+            blockState.update();
+            var inventory = entityContent.inventory();
+            if (blockState instanceof Container container && inventory != null) {
+                ItemStack[] contents = inventory.getContents();
+                container.getInventory().setContents(contents);
+            }
+        }else {
+            blockState.update();
         }
     }
 
