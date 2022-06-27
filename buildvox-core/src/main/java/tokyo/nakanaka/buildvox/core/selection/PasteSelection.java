b91b6323c0acd0a93b27fc6b8c6ad9c94a21a6ca
nakanaka7 <nakanaka7a@gmail.com>
2022-06-28 06:44:12 +0900

    formatted

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selection/PasteSelection.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selection/PasteSelection.java
@@ -10,26 +10,26 @@ import tokyo.nakanaka.buildvox.core.math.transformation.AffineTransformation3d;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3d;
 
 public class PasteSelection extends BlockSelection {
-    private Clipboard clipboard;
-    private Vector3d offset;
-    private AffineTransformation3d clipTrans;
-    private double integrity;
+    private final Clipboard clipboard;
+    private final Vector3d pos;
+    private final AffineTransformation3d clipTrans;
+    private final double integrity;
     private Clipboard backwardClip = new Clipboard();
 
-    private PasteSelection(Region3d region3d, Parallelepiped bound, Clipboard clipboard, Vector3d offset,
+    private PasteSelection(Region3d region3d, Parallelepiped bound, Clipboard clipboard, Vector3d pos,
                            AffineTransformation3d clipTrans, double integrity) {
         super(region3d, bound);
         this.clipboard = clipboard;
-        this.offset = offset;
+        this.pos = pos;
         this.clipTrans = clipTrans;
         this.integrity = integrity;
     }
 
-    public static PasteSelection newInstance(Clipboard clipboard, Vector3d offset, AffineTransformation3d clipTrans) {
-        return newInstance(clipboard, offset, clipTrans, 1);
+    public static PasteSelection newInstance(Clipboard clipboard, Vector3d pos, AffineTransformation3d clipTrans) {
+        return newInstance(clipboard, pos, clipTrans, 1);
     }
 
-    public static PasteSelection newInstance(Clipboard clipboard, Vector3d offset, AffineTransformation3d clipTrans, double integrity) {
+    public static PasteSelection newInstance(Clipboard clipboard, Vector3d pos, AffineTransformation3d clipTrans, double integrity) {
         double maxX = clipboard.maxX() + 1;
         double maxY = clipboard.maxY() + 1;
         double maxZ = clipboard.maxZ() + 1;
@@ -40,15 +40,15 @@ public class PasteSelection extends BlockSelection {
         var bound = new Parallelepiped(cuboid.x1(), cuboid.y1(), cuboid.z1(), cuboid.x2(), cuboid.y2(), cuboid.z2());
         var selection = new Selection(cuboid, bound)
                 .affineTransform(clipTrans)
-                .translate(offset.x(), offset.y(), offset.z());
-        return new PasteSelection(selection.getRegion3d(), selection.getBound(), clipboard, offset, clipTrans, integrity);
+                .translate(pos.x(), pos.y(), pos.z());
+        return new PasteSelection(selection.getRegion3d(), selection.getBound(), clipboard, pos, clipTrans, integrity);
     }
 
     @Override
     public void setForwardBlocks(EditWorld editWorld) {
         Clipboard newBackwardClip = new Clipboard();
         WorldEdits.copy(editWorld, this, Vector3d.ZERO, newBackwardClip);
-        WorldEdits.paste(clipboard, editWorld, offset, clipTrans, integrity);
+        WorldEdits.paste(clipboard, editWorld, pos, clipTrans, integrity);
         backwardClip = newBackwardClip;
     }
 
@@ -60,7 +60,7 @@ public class PasteSelection extends BlockSelection {
     @Override
     public PasteSelection affineTransform(AffineTransformation3d trans) {
         AffineTransformation3d newClipTrans = trans.linear().compose(this.clipTrans);
-        Vector3d newOffset = trans.apply(offset);
+        Vector3d newOffset = trans.apply(pos);
         return newInstance(clipboard, newOffset, newClipTrans, integrity);
     }
 
