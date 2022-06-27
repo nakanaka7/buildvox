ae4748186b71550778ee745d52c2993bedc0354e
nakanaka7 <nakanaka7a@gmail.com>
2022-06-27 10:40:57 +0900

    overloaded replace()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/PlayerEdits.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/PlayerEdits.java
@@ -299,20 +299,33 @@ public class PlayerEdits {
      * @param integrity the integrity of replacing.
      * @return the edit-exit.
      * @throws IllegalArgumentException if integrity is less than 0 or larger than 1.
+     */
+    public static EditExit replace(Player player, Selection sel, VoxelBlock blockFrom, VoxelBlock blockTo, double integrity) {
+        if(integrity < 0 || 1 < integrity) throw new IllegalArgumentException();
+        Selection selTo;
+        if(sel instanceof BlockSelection bs) {
+            selTo = bs.toNonBlock();
+        }else {
+            selTo = sel;
+        }
+        return recordingEdit(
+                player, (editWorld) -> WorldEdits.replace(editWorld, sel, blockFrom, blockTo, integrity), selTo
+        );
+    }
+
+    /**
+     * Replaces blocks.
+     * @param player the player.
+     * @param blockFrom the block to be replaced from
+     * @param blockTo the block to be replaced to
+     * @param integrity the integrity of replacing.
+     * @return the edit-exit.
+     * @throws IllegalArgumentException if integrity is less than 0 or larger than 1.
      * @throws SelectionNotFoundException if a selection is not found
      */
     public static EditExit replace(Player player, VoxelBlock blockFrom, VoxelBlock blockTo, double integrity) {
-        if(integrity < 0 || 1 < integrity) throw new IllegalArgumentException();
-        Selection selFrom = findSelection(player);
-        Selection selTo;
-        if(selFrom instanceof BlockSelection bs) {
-            selTo = bs.toNonBlock();
-        }else {
-            selTo = selFrom;
-        }
-        return recordingEdit(
-            player, (editWorld) -> WorldEdits.replace(editWorld, selFrom, blockFrom, blockTo, integrity), selTo
-        );
+        Selection sel = findSelection(player);
+        return replace(player, sel, blockFrom, blockTo, integrity);
     }
 
     /*
