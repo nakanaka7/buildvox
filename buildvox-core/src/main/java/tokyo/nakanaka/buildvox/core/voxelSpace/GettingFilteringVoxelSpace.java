f8be44dd9000a696977bcec5093823007edbba51
nakanaka7 <nakanaka7a@gmail.com>
2022-06-27 21:36:54 +0900

    formatted

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/voxelSpace/GettingFilteringVoxelSpace.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/voxelSpace/GettingFilteringVoxelSpace.java
@@ -5,28 +5,28 @@ import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import java.util.function.Predicate;
 
 /**
- * The block space which filters the original block when getting it.
+ * The voxel space which filters the original block when getting it.
  * @param <B> block type
  */
 public class GettingFilteringVoxelSpace<B> implements VoxelSpace<B> {
-    private VoxelSpace<B> original;
-    private Predicate<B> predicate;
+    private final VoxelSpace<B> original;
+    private final Predicate<B> get;
 
     /**
-     * Constructs the space by the original block space and predicate of getBlock() method.
+     * Constructs the space by the original voxel space and predicate of getBlock() method.
      * @param original the original world
-     * @param predicate the predicate function for the get blocks in the original space.
+     * @param get the predicate function for the get blocks in the original space.
      */
-    public GettingFilteringVoxelSpace(VoxelSpace<B> original, Predicate<B> predicate) {
+    public GettingFilteringVoxelSpace(VoxelSpace<B> original, Predicate<B> get) {
         this.original = original;
-        this.predicate = predicate;
+        this.get = get;
     }
 
     @Override
     public B getBlock(Vector3i pos) {
         B b = original.getBlock(pos);
         if(b == null)return null;
-        if(predicate.test(b)) return b;
+        if(get.test(b)) return b;
         return null;
     }
 
