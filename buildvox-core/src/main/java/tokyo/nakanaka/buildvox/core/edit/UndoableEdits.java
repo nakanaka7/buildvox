f3071c4eee5b7ffa51e2edacf7dc0b8e1ccce80c
nakanaka7 <nakanaka7a@gmail.com>
2022-06-29 16:38:35 +0900

    initial commit

---
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/UndoableEdits.java
@@ -0,0 +1,71 @@
+package tokyo.nakanaka.buildvox.core.edit;
+
+import javax.swing.undo.UndoableEdit;
+
+public class UndoableEdits {
+    /**
+     * Creates an UndoableEdit from undoRunnable and redoRunnable.
+     * @param undoRunnable a runnable for undo.
+     * @param redoRunnable a runnable for redo.
+     * @return an instance
+     */
+    public static UndoableEdit create(Runnable undoRunnable, Runnable redoRunnable) {
+        return new UndoableEdit() {
+            @Override
+            public void undo() {
+                undoRunnable.run();
+            }
+
+            @Override
+            public boolean canUndo() {
+                return true;
+            }
+
+            @Override
+            public void redo() {
+                redoRunnable.run();
+            }
+
+            @Override
+            public boolean canRedo() {
+                return true;
+            }
+
+            @Override
+            public void die() {
+
+            }
+
+            @Override
+            public boolean addEdit(UndoableEdit anEdit) {
+                return false;
+            }
+
+            @Override
+            public boolean replaceEdit(UndoableEdit anEdit) {
+                return false;
+            }
+
+            @Override
+            public boolean isSignificant() {
+                return true;
+            }
+
+            @Override
+            public String getPresentationName() {
+                return "";
+            }
+
+            @Override
+            public String getUndoPresentationName() {
+                return "";
+            }
+
+            @Override
+            public String getRedoPresentationName() {
+                return "";
+            }
+        };
+    }
+
+}
