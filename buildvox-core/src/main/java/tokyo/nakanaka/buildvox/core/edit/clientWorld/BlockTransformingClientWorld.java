72fec82d59ba593a8f5df33f9931f6c5e1800b82
nakanaka7 <nakanaka7a@gmail.com>
2022-06-29 23:06:09 +0900

    extends ClientWorld

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/clientWorld/BlockTransformingClientWorld.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/clientWorld/BlockTransformingClientWorld.java
@@ -1,27 +1,20 @@
 package tokyo.nakanaka.buildvox.core.edit.clientWorld;
 
 import tokyo.nakanaka.buildvox.core.block.BlockTransformation;
-import tokyo.nakanaka.buildvox.core.edit.voxelSpace.VoxelSpace;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.world.VoxelBlock;
 
-public class BlockTransformingClientWorld implements VoxelSpace<VoxelBlock> {
-    private ClientWorld clientWorld;
-    private BlockTransformation blockTrans;
+public class BlockTransformingClientWorld extends ClientWorld {
+    private final BlockTransformation blockTrans;
 
     public BlockTransformingClientWorld(BlockTransformation blockTrans, ClientWorld clientWorld) {
-        this.clientWorld = clientWorld;
+        super(clientWorld.original, clientWorld.physics);
         this.blockTrans = blockTrans;
     }
 
     @Override
-    public VoxelBlock getBlock(Vector3i pos) {
-        return clientWorld.getBlock(pos);
-    }
-
-    @Override
     public void setBlock(Vector3i pos, VoxelBlock block) {
-        clientWorld.setBlock(pos, block.transform(blockTrans));
+        super.setBlock(pos, block.transform(blockTrans));
     }
 
 }
