593eacf0017ca25d48a9de8c0e02d62055f59ef6
nakanaka7 <nakanaka7a@gmail.com>
2022-06-30 11:42:23 +0900

    changed constructor parameter type from World to ClientWorld

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/clientWorld/RecordingClientWorld.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/clientWorld/RecordingClientWorld.java
@@ -3,32 +3,29 @@ package tokyo.nakanaka.buildvox.core.clientWorld;
 import tokyo.nakanaka.buildvox.core.Clipboard;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.world.VoxelBlock;
-import tokyo.nakanaka.buildvox.core.world.World;
 
 import javax.swing.undo.UndoableEdit;
 import java.util.Set;
 
 /** A client world which records changing. */
 public class RecordingClientWorld extends ClientWorld {
+    private final ClientWorld clientWorld;
     private final Clipboard undoClip = new Clipboard();
     private final Clipboard redoClip = new Clipboard();
 
-    public RecordingClientWorld(World original) {
-        super(original);
-    }
-
-    public RecordingClientWorld(World original, boolean physics) {
-        super(original, physics);
+    public RecordingClientWorld(ClientWorld clientWorld) {
+        super(clientWorld.original, clientWorld.physics);
+        this.clientWorld = clientWorld;
     }
 
     @Override
     public void setBlock(Vector3i pos, VoxelBlock block) {
         if (undoClip.getBlock(pos) == null) {
-            VoxelBlock originalBlock = original.getBlock(pos.x(), pos.y(), pos.z());
+            VoxelBlock originalBlock = getBlock(pos);
             undoClip.setBlock(pos, originalBlock);
         }
         redoClip.setBlock(pos, block);
-        original.setBlock(pos.x(), pos.y(), pos.z(), block, physics);
+        clientWorld.setBlock(pos, block);
     }
 
     public int changingBlockCount() {
@@ -45,14 +42,14 @@ public class RecordingClientWorld extends ClientWorld {
                 Set<Vector3i> blockPosSet = undoClip.blockPosSet();
                 for(Vector3i pos : blockPosSet) {
                     VoxelBlock block = undoClip.getBlock(pos.x(), pos.y(), pos.z());
-                    original.setBlock(pos.x(), pos.y(), pos.z(), block, physics);
+                    clientWorld.setBlock(pos, block);
                 }
             },
             () -> {
                 Set<Vector3i> blockPosSet = redoClip.blockPosSet();
                 for(Vector3i pos : blockPosSet) {
                     VoxelBlock block = redoClip.getBlock(pos.x(), pos.y(), pos.z());
-                    original.setBlock(pos.x(), pos.y(), pos.z(), block, physics);
+                    clientWorld.setBlock(pos, block);
                 }
             }
         );
