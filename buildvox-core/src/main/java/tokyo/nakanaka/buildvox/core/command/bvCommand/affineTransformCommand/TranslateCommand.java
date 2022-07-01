856bab664883449d500bbcf8e1ef12b239078dc6
nakanaka7 <nakanaka7a@gmail.com>
2022-07-02 07:13:01 +0900

    add -s option

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/affineTransformCommand/TranslateCommand.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/affineTransformCommand/TranslateCommand.java
@@ -4,9 +4,13 @@ import picocli.CommandLine;
 import tokyo.nakanaka.buildvox.core.Messages;
 import tokyo.nakanaka.buildvox.core.EditExit;
 import tokyo.nakanaka.buildvox.core.command.NumberCompletionCandidates;
+import tokyo.nakanaka.buildvox.core.command.SelectionShapeParameter;
 import tokyo.nakanaka.buildvox.core.command.bvCommand.BvCommand;
 import tokyo.nakanaka.buildvox.core.edit.PlayerEdits;
 import tokyo.nakanaka.buildvox.core.player.Player;
+import tokyo.nakanaka.buildvox.core.selectionShape.MissingPosException;
+import tokyo.nakanaka.buildvox.core.selectionShape.PosArrayLengthException;
+import tokyo.nakanaka.buildvox.core.selectionShape.SelectionShape;
 
 import java.io.PrintWriter;
 
@@ -24,16 +28,21 @@ public class TranslateCommand implements Runnable {
     private double dy;
     @CommandLine.Parameters(description = "The displacement along z-axis.", completionCandidates = NumberCompletionCandidates.Integer.class)
     private double dz;
+    @CommandLine.Option(names = {"-s", "--shape"}, completionCandidates = SelectionShapeParameter.Candidates.class,
+            converter = SelectionShapeParameter.Converter.class)
+    private SelectionShape shape;
 
     @Override
     public void run() {
         PrintWriter out = commandSpec.commandLine().getOut();
         PrintWriter err = commandSpec.commandLine().getErr();
         Player player = bvCmd.getTargetPlayer();
+        var options = new PlayerEdits.Options();
+        options.shape = shape;
         EditExit editExit;
         try {
-            editExit = PlayerEdits.translate(player, dx, dy, dz);
-        }catch (PlayerEdits.SelectionNotFoundException ex) {
+            editExit = PlayerEdits.translate(player, dx, dy, dz, options);
+        }catch (MissingPosException | PosArrayLengthException ex) {
             err.println(Messages.SELECTION_NULL_ERROR);
             return;
         }
