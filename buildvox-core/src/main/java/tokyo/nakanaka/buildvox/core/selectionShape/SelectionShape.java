e0d5e42a323d9fb9d76ad6c215b53a749046e947
nakanaka7 <nakanaka7a@gmail.com>
2022-07-01 12:42:03 +0900

    javadoc

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/SelectionShape.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/SelectionShape.java
@@ -1,5 +1,6 @@
 package tokyo.nakanaka.buildvox.core.selectionShape;
 
+import tokyo.nakanaka.buildvox.core.command.PosArrayLengthException;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
 
@@ -9,8 +10,9 @@ import tokyo.nakanaka.buildvox.core.selection.Selection;
 public interface SelectionShape {
     /**
      * Creates a selection by the pos-array.
-     * @param posArray the pos-array.
+     * @param posArray the pos-array. All the elements should non-null.
      * @return a selection.
+     * @throws PosArrayLengthException if the pos array length is invalid.
      */
     Selection createSelection(Vector3i[] posArray);
 }
