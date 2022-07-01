252cf24238390d74b099327b2ad25cd1f32a8923
nakanaka7 <nakanaka7a@gmail.com>
2022-07-01 19:44:37 +0900

    implemented convert()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/SelectionShapeParameter.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/SelectionShapeParameter.java
@@ -1,13 +1,14 @@
 package tokyo.nakanaka.buildvox.core.command;
 
-import picocli.CommandLine.ITypeConverter;
+import static picocli.CommandLine.*;
+
+import picocli.CommandLine;
+import tokyo.nakanaka.buildvox.core.ParseUtils;
 import tokyo.nakanaka.buildvox.core.command.mixin.shapeMixin.*;
 import tokyo.nakanaka.buildvox.core.selectionShape.SelectionShape;
+import tokyo.nakanaka.buildvox.core.selectionShape.SelectionShapeImpl;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class SelectionShapeParameter {
     private static final Map<String, Class<? extends ShapeMixin>> shapeMap = new HashMap<>();
@@ -40,7 +41,34 @@ public class SelectionShapeParameter {
     public static class Converter implements ITypeConverter<SelectionShape> {
         @Override
         public SelectionShape convert(String value) throws Exception {
-            return null;
+            ParseUtils.NameStateEntity nse = ParseUtils.parseNameStateEntity(value);
+            if(!nse.entity().isEmpty()) throw new Exception();
+            String name = nse.name();
+            Class<? extends ShapeMixin> shapeClazz = shapeMap.get(name);
+            if(shapeClazz == null) throw new Exception();
+            CommandLine cmdLine = new CommandLine(new DummyCommand());
+            cmdLine.addMixin(name, shapeClazz.getDeclaredConstructor().newInstance());
+            List<String> argsList = new ArrayList<>();
+            String state = nse.state();
+            Map<String, String> stateMap = ParseUtils.parseStateMap(state);
+            for(Map.Entry<String, String> e : stateMap.entrySet()) {
+                argsList.add("--" + e.getKey());
+                argsList.add(e.getValue());
+            }
+            String[] args;
+            args = argsList.toArray(new String[0]);
+            cmdLine.execute(args);
+            Map<String, Object> mixins = cmdLine.getMixins();
+            ShapeMixin shapeMixin = (ShapeMixin) mixins.get(name);
+            return new SelectionShapeImpl(shapeMixin);
+        }
+    }
+
+    @Command
+    private static class DummyCommand implements Runnable {
+        @Override
+        public void run() {
+
         }
     }
 
