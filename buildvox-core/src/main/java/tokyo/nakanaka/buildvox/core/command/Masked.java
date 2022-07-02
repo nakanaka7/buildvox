73ca6625cf6b1f8e165fbb8ab1a1cc3761504da7
nakanaka7 <nakanaka7a@gmail.com>
2022-07-03 07:47:27 +0900

    add masked()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/Masked.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/Masked.java
@@ -5,4 +5,9 @@ import picocli.CommandLine;
 public class Masked {
     @CommandLine.Option(names = {"-m", "--masked"}, description = "Skips background block settings.")
     private boolean masked;
+
+    public boolean masked() {
+        return masked;
+    }
+
 }
