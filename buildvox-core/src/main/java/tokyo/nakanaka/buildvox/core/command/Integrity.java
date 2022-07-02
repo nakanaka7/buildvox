8cc425242633fc5fc10f93a4e3a14ef9745bc1b1
nakanaka7 <nakanaka7a@gmail.com>
2022-07-02 22:30:11 +0900

    add Converter and Candidate

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/Integrity.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/Integrity.java
@@ -3,13 +3,34 @@ package tokyo.nakanaka.buildvox.core.command;
 import picocli.CommandLine;
 import tokyo.nakanaka.buildvox.core.Messages;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Integrity {
     @CommandLine.Option(names = {"-i", "--integrity"}, description = "The integrity of block setting.",
+            converter = Converter.class, completionCandidates = Candidates.class,
             defaultValue = "1")
     private double integrity;
 
     public double integrity() {
         return integrity;
+    }
+
+    private static class Converter implements CommandLine.ITypeConverter<Double> {
+        @Override
+        public Double convert(String value) {
+            var i = Double.parseDouble(value);
+            if(i < 0 || 1 < i) throw new IllegalArgumentException("integrity must be 0..1");
+            return i;
+        }
+    }
+
+    private static class Candidates implements Iterable<String> {
+        @Override
+        public Iterator<String> iterator() {
+            return List.of("0.1", "0.2", "0.3", "0.4", "0.5", "0.6",
+                    "0.7", "0.8", "0.9", "1.0").iterator();
+        }
     }
 
     public void checkValue() {
