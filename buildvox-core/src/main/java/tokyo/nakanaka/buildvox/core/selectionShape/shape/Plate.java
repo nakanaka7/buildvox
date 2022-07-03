f15c72d0c692176350a9681987253cf5dc2d35a8
nakanaka7 <nakanaka7a@gmail.com>
2022-07-03 11:53:28 +0900

    refactored by Thickness

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/shape/Plate.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/shape/Plate.java
@@ -1,20 +1,21 @@
 package tokyo.nakanaka.buildvox.core.selectionShape.shape;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import tokyo.nakanaka.buildvox.core.command.NumberCompletionCandidates;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.property.Axis;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
 import tokyo.nakanaka.buildvox.core.selectionShape.PosArrayLengthException;
 import tokyo.nakanaka.buildvox.core.selectionShape.SelectionCreations;
+import tokyo.nakanaka.buildvox.core.selectionShape.shape.mixin.Thickness;
 
 @Command
 public class Plate implements Shape {
     @Option(names = {"-a", "--axis"})
     private Axis axis = Axis.Y;
-    @Option(names = {"-t", "--thickness"}, defaultValue = "1", completionCandidates = NumberCompletionCandidates.PositiveInteger.class)
-    private int thickness;
+    @CommandLine.Mixin
+    private Thickness thickness;
 
     public static final String DESCRIPTION = "a plate region which corners are pos0 and pos1";
 
@@ -26,7 +27,7 @@ public class Plate implements Shape {
         Vector3i pos0 = posArray[0];
         Vector3i pos1 = posArray[1];
         try{
-            return SelectionCreations.createPlate(pos0, pos1, axis, thickness);
+            return SelectionCreations.createPlate(pos0, pos1, axis, thickness.thickness());
         }catch (IllegalArgumentException ex) {
             throw new IllegalStateException();
         }
