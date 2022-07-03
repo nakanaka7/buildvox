7d810272ca79d221820f0718c23c33e556c63ea0
nakanaka7 <nakanaka7a@gmail.com>
2022-07-03 15:20:54 +0900

    Set candidates 1,2,3

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/shape/mixin/Thickness.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/shape/mixin/Thickness.java
@@ -1,16 +1,25 @@
 package tokyo.nakanaka.buildvox.core.selectionShape.shape.mixin;
 
 import picocli.CommandLine.*;
-import tokyo.nakanaka.buildvox.core.command.NumberCompletionCandidates;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class Thickness {
     @Option(names = {"--thickness"},
-            completionCandidates = NumberCompletionCandidates.PositiveInteger.class
+            completionCandidates = Candidates.class
             ,converter = Converter.class)
     private int thickness = 1;
 
     public int thickness() {
         return thickness;
+    }
+
+    private static class Candidates implements Iterable<String> {
+        @Override
+        public Iterator<String> iterator() {
+            return List.of("1", "2", "3").iterator();
+        }
     }
 
     private static class Converter implements ITypeConverter<Integer> {
