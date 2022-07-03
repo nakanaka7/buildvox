69da3dc66dd93fbc01f8e39dd0239ba68068b1c7
nakanaka7 <nakanaka7a@gmail.com>
2022-07-03 15:07:21 +0900

    improved Candidates.iterator()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/Shape.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/Shape.java
@@ -4,8 +4,7 @@ import picocli.CommandLine.Option;
 import tokyo.nakanaka.buildvox.core.selectionShape.SelectionShape;
 import tokyo.nakanaka.buildvox.core.selectionShape.SelectionShapes;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 import static picocli.CommandLine.ITypeConverter;
 
@@ -21,9 +20,57 @@ public class Shape {
     public static class Candidates implements Iterable<String> {
         @Override
         public Iterator<String> iterator() {
-            Set<String> shapes = SelectionShapes.getShapeNames();
-            return shapes.iterator();
+            Map<String, Map<String, List<String>>> nameKeysValues = getNameKeysValues();
+            Set<String> candidates = new HashSet<>();
+            for(String shapeName : nameKeysValues.keySet()) {
+                candidates.add(shapeName);
+                Map<String, List<String>> keysValues = nameKeysValues.get(shapeName);
+                if(keysValues.size() != 0) {
+                    List<Set<String>> keyValueSetList = new ArrayList<>();
+                    for (String key : keysValues.keySet()) {
+                        Set<String> keyValueSet = new HashSet<>();
+                        List<String> values = keysValues.get(key);
+                        for (String value : values) {
+                            keyValueSet.add(key + "=" + value);
+                        }
+                        keyValueSetList.add(keyValueSet);
+                    }
+                    Set<String> stateCandidates = new HashSet<>();
+                    for (Set<String> keyValueSet : keyValueSetList) {
+                        if (stateCandidates.size() == 0) {
+                            stateCandidates.addAll(keyValueSet);
+                        } else {
+                            for (String keyValue : keyValueSet) {
+                                Set<String> clone = new HashSet<>(stateCandidates);
+                                stateCandidates = new HashSet<>();
+                                for (String e : clone) {
+                                    stateCandidates.add(e + "," + keyValue);
+                                }
+                            }
+                        }
+                    }
+                    for(String state : stateCandidates) {
+                        candidates.add(shapeName + "[" + state + "]");
+                    }
+                }
+            }
+            return candidates.iterator();
         }
+
+        public Map<String, Map<String, List<String>>> getNameKeysValues() {
+            Map<String, Map<String, List<String>>> nameKeysValues = new HashMap<>();
+            Set<String> shapeNames = SelectionShapes.getShapeNames();
+            for(String shapeName : shapeNames) {
+                Map<String, List<String>> keysValues = new HashMap<>();
+                for(String key : SelectionShapes.getKeys(shapeName)) {
+                    keysValues.put(key, SelectionShapes.getValues(shapeName, key));
+                }
+                nameKeysValues.put(shapeName, keysValues);
+            }
+            return nameKeysValues;
+        }
+
+
     }
 
     public static class Converter implements ITypeConverter<SelectionShape> {
