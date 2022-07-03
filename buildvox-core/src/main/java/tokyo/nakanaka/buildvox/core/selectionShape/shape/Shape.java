4e9abb678f3bbb4079d74022f43ae2b97267777d
nakanaka7 <nakanaka7a@gmail.com>
2022-07-03 16:56:33 +0900

    extends SelectionShape

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/shape/Shape.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/shape/Shape.java
@@ -3,8 +3,9 @@ package tokyo.nakanaka.buildvox.core.selectionShape.shape;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
 import tokyo.nakanaka.buildvox.core.selectionShape.PosArrayLengthException;
+import tokyo.nakanaka.buildvox.core.selectionShape.SelectionShape;
 
-public interface Shape {
+public interface Shape extends SelectionShape {
     /**
      * @throws PosArrayLengthException if the size of posData is invalid.
      */
