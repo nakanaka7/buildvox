ca44078b585b8f23e102c856e5fadb77ec15fea3
nakanaka7 <nakanaka7a@gmail.com>
2022-07-03 10:48:02 +0900

    javadoc

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/SelectionShapeImpl.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/SelectionShapeImpl.java
@@ -11,6 +11,12 @@ public class SelectionShapeImpl implements SelectionShape {
         this.shape = shape;
     }
 
+    /**
+     * Creates a selection by the pos-array.
+     * @param posArray the pos-array. All the elements should non-null.
+     * @return a selection.
+     * @throws PosArrayLengthException if the pos array length is invalid.
+     */
     @Override
     public Selection createSelection(Vector3i[] posArray) {
         return shape.createSelection(posArray);
