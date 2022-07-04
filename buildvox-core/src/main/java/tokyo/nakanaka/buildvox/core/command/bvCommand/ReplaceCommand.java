cf45cd8ccf173f1b6164289aae94f58cc192e570
nakanaka7 <nakanaka7a@gmail.com>
2022-07-04 17:13:44 +0900

    refactored by Block.Candidates.class and Shape

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/ReplaceCommand.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/ReplaceCommand.java
@@ -1,17 +1,15 @@
 package tokyo.nakanaka.buildvox.core.command.bvCommand;
 
 import picocli.CommandLine.*;
-import tokyo.nakanaka.buildvox.core.Messages;
-import tokyo.nakanaka.buildvox.core.command.mixin.Block;
 import tokyo.nakanaka.buildvox.core.EditExit;
+import tokyo.nakanaka.buildvox.core.Messages;
+import tokyo.nakanaka.buildvox.core.block.VoxelBlock;
+import tokyo.nakanaka.buildvox.core.command.mixin.Block;
 import tokyo.nakanaka.buildvox.core.command.mixin.Integrity;
 import tokyo.nakanaka.buildvox.core.command.mixin.Shape;
 import tokyo.nakanaka.buildvox.core.edit.PlayerEdits;
 import tokyo.nakanaka.buildvox.core.player.Player;
 import tokyo.nakanaka.buildvox.core.selectionShape.PosArrayLengthException;
-import tokyo.nakanaka.buildvox.core.selectionShape.SelectionShape;
-import tokyo.nakanaka.buildvox.core.system.BuildVoxSystem;
-import tokyo.nakanaka.buildvox.core.block.VoxelBlock;
 
 import java.io.PrintWriter;
 
@@ -24,47 +22,32 @@ public class ReplaceCommand implements Runnable {
     @ParentCommand
     private BvCommand bvCmd;
     @Parameters(arity = "1",
-            description = "The block type to replace from.", completionCandidates = Block.Candidates.class)
-    private String blockFrom;
+            description = "The block type to replace from.", completionCandidates = Block.Candidates.class,
+            converter = Block.Converter.class)
+    private VoxelBlock blockFrom;
     @Parameters(arity = "1",
-            description = "The block type to replace to.", completionCandidates = Block.Candidates.class)
-    private String blockTo;
+            description = "The block type to replace to.", completionCandidates = Block.Candidates.class,
+            converter = Block.Converter.class)
+    private VoxelBlock blockTo;
     @Mixin
     private Integrity integrity;
-    @Option(names = {"-s", "--shape"}, completionCandidates = Shape.Candidates.class,
-            converter = Shape.Converter.class)
-    private SelectionShape shape;
+    @Mixin
+    private Shape shape;
 
     @Override
     public void run() {
         PrintWriter out = commandSpec.commandLine().getOut();
         PrintWriter err = commandSpec.commandLine().getErr();
         Player player = bvCmd.getPlayer();
-        VoxelBlock bFrom;
-        try {
-            bFrom = BuildVoxSystem.parseBlock(blockFrom);
-        }catch (IllegalArgumentException e) {
-            err.println(Messages.ofBlockParseError(blockFrom));
-            return;
-        }
-        VoxelBlock bTo;
-        try {
-            bTo = BuildVoxSystem.parseBlock(blockTo);
-        }catch (IllegalArgumentException e) {
-            err.println(Messages.ofBlockParseError(blockTo));
-            return;
-        }
         var options = new PlayerEdits.Options();
         options.integrity = integrity.integrity();
-        options.shape = shape;
-        EditExit exit;
+        options.shape = shape.shape();
         try {
-            exit = PlayerEdits.replace(player, bFrom, bTo, options);
+            EditExit exit = PlayerEdits.replace(player, blockFrom, blockTo, options);
+            out.println(Messages.ofSetExit(exit));
         }catch (PlayerEdits.MissingPosException | PosArrayLengthException ex) {
             err.println(Messages.SELECTION_NULL_ERROR);
-            return;
         }
-        out.println(Messages.ofSetExit(exit));
     }
 
 }
