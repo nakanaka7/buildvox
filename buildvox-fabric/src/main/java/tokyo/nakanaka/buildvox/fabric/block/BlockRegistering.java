990532bf803425c825718ad8f0f21d19b7a0cc68
nakanaka7 <nakanaka7a@gmail.com>
2022-07-07 20:34:47 +0900

    refactored

--- buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/BlockRegistering.java
+++ buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/BlockRegistering.java
@@ -1,8 +1,10 @@
 package tokyo.nakanaka.buildvox.fabric.block;
 
+import net.minecraft.block.StairsBlock;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import tokyo.nakanaka.buildvox.core.NamespacedId;
+import tokyo.nakanaka.buildvox.core.block.Block;
 import tokyo.nakanaka.buildvox.core.system.BuildVoxSystem;
 
 import static tokyo.nakanaka.buildvox.fabric.NamespacedIds.createId;
@@ -11,10 +13,20 @@ public class BlockRegistering {
     private BlockRegistering() {
     }
 
+    /** Register all the blocks into the BuildVoxSystem block registry . */
     public static void registerBlocks() {
-        for(Identifier blockId0 : Registry.BLOCK.getIds()) {
-            NamespacedId id = createId(blockId0);
-            BuildVoxSystem.getBlockRegistry().register(new FabricBlock(id));
+        var registry0 = Registry.BLOCK;
+        var registry = BuildVoxSystem.getBlockRegistry();
+        for(Identifier id0 : registry0.getIds()) {
+            NamespacedId id = createId(id0);
+            net.minecraft.block.Block block0 = registry0.get(id0);
+            Block<?, ?> block;
+            if(block0 instanceof StairsBlock) {
+                block = new FabricBlock(id);
+            }else {
+                block = new FabricBlock(id);
+            }
+            registry.register(block);
         }
     }
 
