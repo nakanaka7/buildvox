dfe457a1b7ad0bdd8f135a77e9d2e8151fb6575e
nakanaka7 <nakanaka7a@gmail.com>
2022-07-07 16:09:03 +0900

    overloaded constructor which parameter type is BlockState

--- buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/FabricBlockState.java
+++ buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/FabricBlockState.java
@@ -1,18 +1,27 @@
 package tokyo.nakanaka.buildvox.fabric.block;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.state.property.Property;
 import tokyo.nakanaka.buildvox.core.ParseUtils;
 import tokyo.nakanaka.buildvox.core.block.Block;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 /* internal */
 public class FabricBlockState implements Block.State {
     private Map<String, String> stateMap;
 
     public FabricBlockState(Map<String, String> stateMap) {
+        this.stateMap = stateMap;
+    }
+
+    public FabricBlockState(BlockState blockState) {
+        Collection<Property<?>> properties0 = blockState.getProperties();
+        Map<String, String> stateMap = new HashMap<>();
+        for(var key0 : properties0){
+            Object value0 = blockState.get(key0);
+            stateMap.put(key0.getName().toLowerCase(), value0.toString().toLowerCase());
+        }
         this.stateMap = stateMap;
     }
 
