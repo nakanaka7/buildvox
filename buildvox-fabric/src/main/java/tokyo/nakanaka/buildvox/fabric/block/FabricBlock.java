399c036963e62fefacbc85d5f903b622de5d8cf7
nakanaka7 <nakanaka7a@gmail.com>
2022-07-07 16:54:10 +0900

    refactored transformState()

--- buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/FabricBlock.java
+++ buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/FabricBlock.java
@@ -6,8 +6,6 @@ import tokyo.nakanaka.buildvox.core.block.Block;
 import tokyo.nakanaka.buildvox.core.block.BlockTransformation;
 import tokyo.nakanaka.buildvox.core.block.VoxelBlock;
 
-import java.util.Map;
-
 /*
  * internal
  * Block implementation class for Fabric platform.
@@ -26,11 +24,10 @@ public class FabricBlock implements Block<FabricBlockState, FabricBlockEntity> {
 
     @Override
     public FabricBlockState transformState(FabricBlockState state, BlockTransformation blockTrans) {
-        VoxelBlock block = new VoxelBlock(id, new FabricBlockState(state.getStateMap()));
+        VoxelBlock block = new VoxelBlock(id, state);
         BlockState blockState = BlockUtils.createBlockState(block);
         BlockState transState = FabricBlockState.transform(blockState, blockTrans);
-        Map<String, String> transMap = BlockUtils.createFabricBlockState(transState).getStateMap();
-        return new FabricBlockState(transMap);
+        return new FabricBlockState(transState);
     }
 
     public FabricBlockState parseState(String s) {
