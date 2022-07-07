211bd47a10588d85edc89fabc6e8ff94096d21c1
nakanaka7 <nakanaka7a@gmail.com>
2022-07-07 18:46:35 +0900

    moved parseBlockState()

--- buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/BlockUtils.java
+++ buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/BlockUtils.java
@@ -1,28 +1,7 @@
 package tokyo.nakanaka.buildvox.fabric.block;
 
-import com.mojang.brigadier.StringReader;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.block.BlockState;
-import net.minecraft.command.argument.BlockStateArgument;
-import net.minecraft.command.argument.BlockStateArgumentType;
-
 public class BlockUtils {
     private BlockUtils() {
-    }
-
-    /**
-     * Parses a BlockState
-     * @throws IllegalArgumentException if fails to create.
-     */
-    public static BlockState parseBlockState(String s) {
-        StringReader strReader = new StringReader(s);
-        BlockStateArgument blockStateArg;
-        try {
-            blockStateArg = new BlockStateArgumentType().parse(strReader);
-        } catch (CommandSyntaxException e) {
-            throw new IllegalArgumentException("Cannot parse:" + s);
-        }
-        return blockStateArg.getBlockState();
     }
 
 }
--- buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/FabricBlock.java
+++ buildvox-fabric/src/main/java/tokyo/nakanaka/buildvox/fabric/block/FabricBlock.java
@@ -1,6 +1,10 @@
 package tokyo.nakanaka.buildvox.fabric.block;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.block.BlockState;
+import net.minecraft.command.argument.BlockStateArgument;
+import net.minecraft.command.argument.BlockStateArgumentType;
 import tokyo.nakanaka.buildvox.core.NamespacedId;
 import tokyo.nakanaka.buildvox.core.block.Block;
 import tokyo.nakanaka.buildvox.core.block.BlockTransformation;
@@ -16,6 +20,8 @@ public class FabricBlock implements Block<FabricBlockState, FabricBlockEntity> {
         this.id = id;
     }
 
+
+
     @Override
     public NamespacedId getId() {
         return id;
@@ -30,8 +36,23 @@ public class FabricBlock implements Block<FabricBlockState, FabricBlockEntity> {
     @Override
     public FabricBlockState parseState(String s) {
         String t = id.toString() + "[" + s + "]";
-        var u = BlockUtils.parseBlockState(t);
+        var u = parseBlockState(t);
         return new FabricBlockState(u);
+    }
+
+    /**
+     * Parses a BlockState
+     * @throws IllegalArgumentException if fails to create.
+     */
+    private static BlockState parseBlockState(String s) {
+        StringReader strReader = new StringReader(s);
+        BlockStateArgument blockStateArg;
+        try {
+            blockStateArg = new BlockStateArgumentType().parse(strReader);
+        } catch (CommandSyntaxException e) {
+            throw new IllegalArgumentException("Cannot parse:" + s);
+        }
+        return blockStateArg.getBlockState();
     }
 
 }
