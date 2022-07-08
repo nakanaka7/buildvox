700076f882589ba27275a00c04642d4a6936b70f
nakanaka7 <nakanaka7a@gmail.com>
2022-07-09 06:18:21 +0900

    add hollow option

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Torus.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Torus.java
@@ -1,12 +1,16 @@
 package tokyo.nakanaka.buildvox.core.selectionShape;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.Hollow;
 import tokyo.nakanaka.buildvox.core.selectionShape.util.SelectionCreations;
 
 @Command(description = "a torus region in the cuboid by pos0 and pos1")
 public class Torus implements SelectionShape {
+    @CommandLine.Mixin
+    private Hollow hollow;
 
     @Override
     public Selection createSelection(Vector3i[] posArray) {
@@ -15,7 +19,11 @@ public class Torus implements SelectionShape {
         }
         Vector3i pos0 = posArray[0];
         Vector3i pos1 = posArray[1];
-        return SelectionCreations.createTorus(pos0, pos1);
+        if(!hollow.hollow()) {
+            return SelectionCreations.createTorus(pos0, pos1);
+        }else{
+            return SelectionCreations.createHollowTorus(pos0, pos1, 1);
+        }
     }
 
 }
