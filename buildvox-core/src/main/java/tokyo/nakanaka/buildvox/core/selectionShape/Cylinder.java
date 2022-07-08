192c52e2a4ee60a644db90e3a88fa470aeae73bd
nakanaka7 <nakanaka7a@gmail.com>
2022-07-08 20:16:02 +0900

    add hollow option

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Cylinder.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Cylinder.java
@@ -1,15 +1,21 @@
 package tokyo.nakanaka.buildvox.core.selectionShape;
 
-import picocli.CommandLine.*;
-import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
+import picocli.CommandLine.Option;
 import tokyo.nakanaka.buildvox.core.Axis;
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.Hollow;
 import tokyo.nakanaka.buildvox.core.selectionShape.util.SelectionCreations;
 
 @Command(description = "a cylinder region in the cuboid by pos0 and pos1")
 public class Cylinder implements SelectionShape {
     @Option(names = {"--axis"})
     private Axis axis = Axis.Y;
+
+    @Mixin
+    private Hollow hollow;
 
     @Override
     public Selection createSelection(Vector3i[] posArray) {
@@ -18,7 +24,11 @@ public class Cylinder implements SelectionShape {
         }
         Vector3i pos0 = posArray[0];
         Vector3i pos1 = posArray[1];
-        return SelectionCreations.createCylinder(pos0, pos1, axis);
+        if(!hollow.hollow()) {
+            return SelectionCreations.createCylinder(pos0, pos1, axis);
+        }else {
+            return SelectionCreations.createHollowCylinder(pos0, pos1, axis, 1);
+        }
     }
 
 }
