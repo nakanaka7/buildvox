1dd9c8907e38b4b43b9fb3bd829c541d6a6775a9
nakanaka7 <nakanaka7a@gmail.com>
2022-07-09 13:53:49 +0900

    add hollow option

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Cuboid.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Cuboid.java
@@ -1,20 +1,28 @@
 package tokyo.nakanaka.buildvox.core.selectionShape;
 
+import picocli.CommandLine;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.Hollow;
 import tokyo.nakanaka.buildvox.core.selectionShape.util.SelectionCreations;
 
 import static picocli.CommandLine.Command;
 
 @Command(description = "a cuboid by pos0 and pos1")
 public class Cuboid implements SelectionShape {
+    @CommandLine.Mixin
+    private Hollow hollow;
 
     @Override
     public Selection createSelection(Vector3i[] posArray) {
         if(posArray.length != 2)throw new PosArrayLengthException(2);
         Vector3i pos0 = posArray[0];
         Vector3i pos1 = posArray[1];
-        return SelectionCreations.createCuboid(pos0, pos1);
+        if(!hollow.hollow()) {
+            return SelectionCreations.createCuboid(pos0, pos1);
+        }else {
+            return SelectionCreations.createHollowCuboid(pos0, pos1, 1);
+        }
     }
 
 }
