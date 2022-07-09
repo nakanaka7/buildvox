c90b8e18f89238235c59bd38f6945504ac685980
nakanaka7 <nakanaka7a@gmail.com>
2022-07-09 17:08:53 +0900

    initial commit

---
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/HollowEllipse.java
@@ -0,0 +1,23 @@
+package tokyo.nakanaka.buildvox.core.selectionShape;
+
+import picocli.CommandLine;
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
+import tokyo.nakanaka.buildvox.core.selection.Selection;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.Thickness;
+import tokyo.nakanaka.buildvox.core.selectionShape.util.SelectionCreations;
+
+public class HollowEllipse implements SelectionShape {
+    @CommandLine.Mixin
+    private Thickness thickness;
+
+    @Override
+    public Selection createSelection(Vector3i[] posArray) {
+        if (posArray.length != 2) {
+            throw new PosArrayLengthException(2);
+        }
+        Vector3i pos0 = posArray[0];
+        Vector3i pos1 = posArray[1];
+        return SelectionCreations.createHollowEllipse(pos0, pos1, thickness.thickness());
+    }
+
+}
