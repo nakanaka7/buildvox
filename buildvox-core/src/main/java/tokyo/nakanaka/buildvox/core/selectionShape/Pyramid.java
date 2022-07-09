1818632bc670c759a4544a53ec595b3f4eba0fbc
nakanaka7 <nakanaka7a@gmail.com>
2022-07-09 23:03:37 +0900

    refactored by Axis mixin

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Pyramid.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/Pyramid.java
@@ -1,16 +1,15 @@
 package tokyo.nakanaka.buildvox.core.selectionShape;
 
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import tokyo.nakanaka.buildvox.core.Axis;
+import picocli.CommandLine.*;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.Axis;
 import tokyo.nakanaka.buildvox.core.selectionShape.util.SelectionCreations;
 
 @Command(description = "a pyramid in the cuboid by pos0 and pos1")
 public class Pyramid implements SelectionShape {
-    @Option(names = {"--axis"})
-    private Axis axis = Axis.Y;
+    @Mixin
+    private Axis axis;
 
     @Override
     public Selection createSelection(Vector3i[] posArray) {
@@ -19,7 +18,7 @@ public class Pyramid implements SelectionShape {
         }
         Vector3i pos0 = posArray[0];
         Vector3i pos1 = posArray[1];
-        return SelectionCreations.createPyramid(pos0, pos1, axis);
+        return SelectionCreations.createPyramid(pos0, pos1, axis.axis());
     }
 
 }
