89f31d9bc699cd9af12f547f25a813368b45145e
nakanaka7 <nakanaka7a@gmail.com>
2022-07-09 16:56:49 +0900

    initial commit

---
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/HollowCone.java
@@ -0,0 +1,26 @@
+package tokyo.nakanaka.buildvox.core.selectionShape;
+
+import picocli.CommandLine.Mixin;
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
+import tokyo.nakanaka.buildvox.core.selection.Selection;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.Axis;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.Thickness;
+import tokyo.nakanaka.buildvox.core.selectionShape.util.SelectionCreations;
+
+public class HollowCone implements SelectionShape {
+    @Mixin
+    private Axis axis;
+    @Mixin
+    private Thickness thickness;
+
+    @Override
+    public Selection createSelection(Vector3i[] posArray) {
+        if (posArray.length != 2) {
+            throw new PosArrayLengthException(2);
+        }
+        Vector3i pos0 = posArray[0];
+        Vector3i pos1 = posArray[1];
+        return SelectionCreations.createHollowCone(pos0, pos1, axis.axis(), thickness.thickness());
+    }
+
+}
