82aa8145036c3eb47e7c0d645d2ce9ef7d3fe4f5
nakanaka7 <nakanaka7a@gmail.com>
2022-07-09 16:29:25 +0900

    initial commit

---
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/selectionShape/HollowCylinder.java
@@ -0,0 +1,25 @@
+package tokyo.nakanaka.buildvox.core.selectionShape;
+
+import picocli.CommandLine.Mixin;
+import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
+import tokyo.nakanaka.buildvox.core.selection.Selection;
+import tokyo.nakanaka.buildvox.core.selectionShape.mixin.*;
+import tokyo.nakanaka.buildvox.core.selectionShape.util.SelectionCreations;
+
+public class HollowCylinder implements SelectionShape {
+    @Mixin
+    private Axis axis;
+    @Mixin
+    private Thickness thickness;
+
+    @Override
+    public Selection createSelection(Vector3i[] posArray) {
+        if (posArray.length != 2) {
+            throw new PosArrayLengthException(2);
+        }
+        Vector3i pos0 = posArray[0];
+        Vector3i pos1 = posArray[1];
+        return SelectionCreations.createHollowCylinder(pos0, pos1, axis.axis(), thickness.thickness());
+    }
+
+}
