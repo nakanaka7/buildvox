6d8d599c85cbdddc55f4663c44185512942889f5
nakanaka7 <nakanaka7a@gmail.com>
2022-07-13 19:28:10 +0900

    initial commit

---
+++ buildvox-bukkit/src/main/java/tokyo/nakanaka/buildvox/bukkit/block/BukkitBlockEntity.java
@@ -0,0 +1,32 @@
+package tokyo.nakanaka.buildvox.bukkit.block;
+
+import tokyo.nakanaka.buildvox.core.block.Block;
+import tokyo.nakanaka.buildvox.core.block.EntityImpl;
+
+import java.util.Objects;
+
+public class BukkitBlockEntity implements Block.Entity {
+    private Object obj;
+
+    public BukkitBlockEntity(Object obj) {
+        this.obj = obj;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BukkitBlockEntity entity = (BukkitBlockEntity) o;
+        return obj.equals(entity.obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(obj);
+    }
+
+}
