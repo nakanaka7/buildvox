48a82360890ca1d8796a6a3dba28393b71e06732
nakanaka7 <nakanaka7a@gmail.com>
2022-07-18 08:10:17 +0900

    switched to PlayerEdits.repeat() with blockSettingOptions parameter

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/RepeatCommand.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/RepeatCommand.java
@@ -1,10 +1,9 @@
 package tokyo.nakanaka.buildvox.core.command.bvCommand;
 
 import picocli.CommandLine.*;
+import tokyo.nakanaka.buildvox.core.BlockSettingOptions;
 import tokyo.nakanaka.buildvox.core.EditExit;
 import tokyo.nakanaka.buildvox.core.Messages;
-import tokyo.nakanaka.buildvox.core.command.mixin.Integrity;
-import tokyo.nakanaka.buildvox.core.command.mixin.Masked;
 import tokyo.nakanaka.buildvox.core.command.mixin.Shape;
 import tokyo.nakanaka.buildvox.core.command.util.NumberCompletionCandidates;
 import tokyo.nakanaka.buildvox.core.edit.PlayerEdits;
@@ -28,23 +27,17 @@ public class RepeatCommand implements Runnable {
     @Parameters(description = "The count along z-axis.", completionCandidates = NumberCompletionCandidates.Integer.class)
     private int countZ;
     @Mixin
-    private Integrity integrity;
-    @Mixin
-    private Masked masked;
-    @Mixin
     private Shape shape;
+    @Mixin
+    private BlockSettingOptions blockSettingOptions;
 
     @Override
     public void run() {
         PrintWriter out = commandSpec.commandLine().getOut();
         PrintWriter err = commandSpec.commandLine().getErr();
         Player player = bvCmd.getPlayer();
-        PlayerEdits.Options options = new PlayerEdits.Options();
-        options.integrity = integrity.integrity();
-        options.masked = masked.masked();
-        options.shape = shape.shape();
         try {
-            EditExit exit = PlayerEdits.repeat(player, countX, countY, countZ, options);
+            EditExit exit = PlayerEdits.repeat(player, countX, countY, countZ, shape.shape(), blockSettingOptions);
             out.println(Messages.ofSetExit(exit));
         }catch (PlayerEdits.MissingPosException ex) {
             err.println(Messages.MISSING_POS_ERROR);
