ddf0901777140557918972e52be29c58e86e9745
nakanaka7 <nakanaka7a@gmail.com>
2022-07-17 19:35:59 +0900

    refactored fill() with new FillSelection constructor

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/PlayerEdits.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/edit/PlayerEdits.java
@@ -398,8 +398,7 @@ public class PlayerEdits {
         if(sel == null) {
             sel = createPosArraySelection(player.getPosArrayClone(), shape);
         }
-        FillSelection fillSelection = new FillSelection.Builder(block, sel).build();
-        fillSelection.setOptions(blockSettingOptions);
+        FillSelection fillSelection = new FillSelection(sel, block, blockSettingOptions);
         PlayerClientWorld pcw = new PlayerClientWorld(player);
         fillSelection.setForwardBlocks(pcw);
         pcw.setSelection(fillSelection);
