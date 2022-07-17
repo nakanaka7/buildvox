89f5924180c57970c6617c91fc9579412450d59e
nakanaka7 <nakanaka7a@gmail.com>
2022-07-17 17:34:46 +0900

    deleted Builder

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/brushSource/BrushSource.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/brushSource/BrushSource.java
@@ -17,34 +17,6 @@ public class BrushSource {
         this(clipboard, new BlockSettingOptions());
     }
 
-    private BrushSource(Builder builder) {
-        this.clipboard = builder.clipboard;
-        this.options = new BlockSettingOptions();
-        this.options.setIntegrity(builder.integrity);
-        this.options.setMasked(builder.mask);
-    }
-
-    public static class Builder {
-        private Clipboard clipboard;
-        private double integrity = 1;
-        private boolean mask;
-        @Deprecated
-        public Builder(Clipboard clipboard) {
-            this.clipboard = clipboard;
-        }
-        public Builder integrity(double integrity) {
-            this.integrity = integrity;
-            return this;
-        }
-        public Builder masked(boolean mask) {
-            this.mask = mask;
-            return this;
-        }
-        public BrushSource build() {
-            return new BrushSource(this);
-        }
-    }
-
     public Clipboard getClipboard() {
         return clipboard;
     }
