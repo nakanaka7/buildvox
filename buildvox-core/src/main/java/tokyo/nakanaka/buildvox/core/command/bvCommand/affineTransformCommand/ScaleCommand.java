7340a4a00fea145d26d32d1403926577331d388b
nakanaka7 <nakanaka7a@gmail.com>
2022-07-18 12:36:26 +0900

    switched to PlayerEdits.scale() with blockSettingOptions parameter

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/affineTransformCommand/ScaleCommand.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/affineTransformCommand/ScaleCommand.java
@@ -1,11 +1,10 @@
 package tokyo.nakanaka.buildvox.core.command.bvCommand.affineTransformCommand;
 
 import picocli.CommandLine.*;
+import tokyo.nakanaka.buildvox.core.BlockSettingOptions;
 import tokyo.nakanaka.buildvox.core.EditExit;
 import tokyo.nakanaka.buildvox.core.Messages;
 import tokyo.nakanaka.buildvox.core.command.bvCommand.BvCommand;
-import tokyo.nakanaka.buildvox.core.command.mixin.Integrity;
-import tokyo.nakanaka.buildvox.core.command.mixin.Masked;
 import tokyo.nakanaka.buildvox.core.command.mixin.Pos;
 import tokyo.nakanaka.buildvox.core.command.mixin.Shape;
 import tokyo.nakanaka.buildvox.core.command.util.NumberCompletionCandidates;
@@ -33,11 +32,10 @@ public class ScaleCommand implements Runnable {
     @Mixin
     private Pos pos;
     @Mixin
-    private Integrity integrity;
-    @Mixin
-    private Masked masked;
-    @Mixin
     private Shape shape;
+    @Mixin
+    private BlockSettingOptions blockSettingOptions;
+
 
     @Override
     public void run() {
@@ -49,12 +47,8 @@ public class ScaleCommand implements Runnable {
             return;
         }
         Vector3d pos = this.pos.toVector3d(bvCmd.getExecutionPos());
-        var options = new PlayerEdits.Options();
-        options.integrity = integrity.integrity();
-        options.masked = masked.masked();
-        options.shape = shape.shape();
         try {
-            EditExit editExit = PlayerEdits.scale(player, factorX, factorY, factorZ, pos, options);
+            EditExit editExit = PlayerEdits.scale(player, factorX, factorY, factorZ, pos, shape.shape(), blockSettingOptions);
             out.println(Messages.ofSetExit(editExit));
         }catch (PlayerEdits.MissingPosException ex) {
             err.println(Messages.MISSING_POS_ERROR);
