47f6abddec87d1cb7dd544db15fabac3b7ca5259
nakanaka7 <nakanaka7a@gmail.com>
2022-07-18 19:37:54 +0900

    delegates updateParticleGui() to ParticleGuis.updateParticleGui()

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/player/Player.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/player/Player.java
@@ -5,12 +5,9 @@ import tokyo.nakanaka.buildvox.core.World;
 import tokyo.nakanaka.buildvox.core.block.VoxelBlock;
 import tokyo.nakanaka.buildvox.core.brushSource.BrushSource;
 import tokyo.nakanaka.buildvox.core.brushSource.BrushSourceClipboards;
-import tokyo.nakanaka.buildvox.core.math.region3d.Parallelepiped;
 import tokyo.nakanaka.buildvox.core.math.vector.Vector3i;
-import tokyo.nakanaka.buildvox.core.particleGui.Color;
 import tokyo.nakanaka.buildvox.core.particleGui.ParticleGui;
-import tokyo.nakanaka.buildvox.core.selection.FillSelection;
-import tokyo.nakanaka.buildvox.core.selection.PasteSelection;
+import tokyo.nakanaka.buildvox.core.particleGui.ParticleGuis;
 import tokyo.nakanaka.buildvox.core.selection.Selection;
 import tokyo.nakanaka.buildvox.core.system.BuildVoxSystem;
 
@@ -188,64 +185,7 @@ public class Player {
     }
 
     private void updateParticleGui() {
-        if(!particleGuiVisible) return;
-        if(particleGui == null)return;
-        particleGui.clearAllLines();
-        World posOrSelectionWorld = getEditWorld();
-        if(posOrSelectionWorld == null)return;
-        Selection selection = getSelection();
-        if(selection != null){
-            Color color;
-            if(selection instanceof PasteSelection){
-                color = Color.YELLOW;
-            }else if(selection instanceof FillSelection){
-                color = Color.LIME;
-            }else{
-                color = Color.MAGENTA;
-            }
-            particleGui.addParallelepipedLines(color, posOrSelectionWorld, selection.getBound());
-        }
-        Vector3i[] posData = getPosArrayClone();
-        for(int i = 0; i < posData.length; ++ i){
-            Vector3i pos = posData[i];
-            if(pos != null){
-                Color color;
-                if(i == 0){
-                    color = Color.RED;
-                }else if(i == 1){
-                    color = Color.BLUE;
-                }else if(i == 2){
-                    color = Color.YELLOW;
-                }else if(i == 3){
-                    color = Color.LIME;
-                }else{
-                    throw new InternalError();
-                }
-                particleGui.addBlockLines(color, posOrSelectionWorld, (int)Math.floor(pos.x()), (int)Math.floor(pos.y()), (int)Math.floor(pos.z()));
-                for(int j = i + 1; j < posData.length; ++ j){
-                    Vector3i posJ = posData[j];
-                    if(posJ != null){
-                        particleGui.addLine(Color.CYAN, posOrSelectionWorld,
-                                pos.x() + 0.5, pos.y() + 0.5, pos.z() + 0.5,
-                                posJ.x() + 0.5, posJ.y() + 0.5, posJ.z() + 0.5);
-                    }
-                }
-            }
-        }
-        if(posData.length == 2){
-            Vector3i pos0 = posData[0];
-            Vector3i pos1 = posData[1];
-            if(pos0 != null && pos1 != null){
-                double px = Math.max(pos0.x(), pos1.x()) + 1;
-                double py = Math.max(pos0.y(), pos1.y()) + 1;
-                double pz = Math.max(pos0.z(), pos1.z()) + 1;
-                double nx = Math.min(pos0.x(), pos1.x());
-                double ny = Math.min(pos0.y(), pos1.y());
-                double nz = Math.min(pos0.z(), pos1.z());
-                Parallelepiped parallelepiped = new Parallelepiped(px, py, pz, nx, ny, nz);
-                particleGui.addParallelepipedLines(Color.CYAN, posOrSelectionWorld, parallelepiped);
-            }
-        }
+        ParticleGuis.updateParticleGui(this);
     }
 
 }
