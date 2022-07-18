204443b4a4a44af17f20773f7309ae6ddfafa671
nakanaka7 <nakanaka7a@gmail.com>
2022-07-18 22:37:36 +0900

    add constructor

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/particleGui/PlayerParticleGui.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/particleGui/PlayerParticleGui.java
@@ -1,8 +1,15 @@
 package tokyo.nakanaka.buildvox.core.particleGui;
 
+import tokyo.nakanaka.buildvox.core.player.Player;
+
 public class PlayerParticleGui {
+    private Player player;
     private boolean particleGuiVisible;
     private ParticleGui particleGui;
+
+    public PlayerParticleGui(Player player) {
+        this.player = player;
+    }
 
     public boolean isParticleGuiVisible() {
         return particleGuiVisible;
