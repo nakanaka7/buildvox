deb2d031c968f67be4a8596063adb729aca650b5
nakanaka7 <nakanaka7a@gmail.com>
2022-07-23 15:49:22 +0900

    changed size filed to diameter

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/brushBindCommand/SphereCommand.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/bvCommand/brushBindCommand/SphereCommand.java
@@ -6,11 +6,10 @@ import tokyo.nakanaka.buildvox.core.brushSource.BrushSource;
 import tokyo.nakanaka.buildvox.core.brushSource.BrushSourceClipboards;
 import tokyo.nakanaka.buildvox.core.command.bvCommand.BvCommand;
 import tokyo.nakanaka.buildvox.core.command.mixin.Block;
+import tokyo.nakanaka.buildvox.core.command.mixin.PositiveInteger;
 import tokyo.nakanaka.buildvox.core.player.Player;
 
 import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.List;
 
 @Command(name = "sphere")
 public class SphereCommand implements Runnable {
@@ -23,45 +22,21 @@ public class SphereCommand implements Runnable {
     @Mixin
     private Block block;
 
-    @Mixin
-    private Size size;
+    @Parameters(description = "diameter(default = 3)",
+            defaultValue = "3",
+            completionCandidates = PositiveInteger.PositiveIntegerCandidates.class,
+            converter = PositiveInteger.PositiveIntegerConverter.class)
+    private int diameter;
 
     @Override
     public void run() {
         PrintWriter out = spec.commandLine().getOut();
         BvCommand bvCmd = brushBindCmd.getBvCommand();
         Player player = bvCmd.getPlayer();
-        Clipboard clipboard = BrushSourceClipboards.createSphere(block.block(), size.size());
+        Clipboard clipboard = BrushSourceClipboards.createSphere(block.block(), diameter);
         BrushSource src = new BrushSource(clipboard, brushBindCmd.getBlockSettingOptions());
         player.setBrushSource(src);
-        out.println("Bound sphere(" + "block:" + block.block().getBlockId().name() + ",size:" + size.size() + ") to brush.");
-    }
-
-    @Command
-    private static class Size {
-        @Parameters(completionCandidates = Candidates.class, converter = Converter.class)
-        private int size;
-
-        public int size() {
-            return size;
-        }
-
-        private static class Candidates implements Iterable<String> {
-            @Override
-            public Iterator<String> iterator() {
-                return List.of("1", "2", "3", "4", "5", "6").iterator();
-            }
-        }
-
-        private static class Converter implements ITypeConverter<Integer> {
-            @Override
-            public Integer convert(String value) {
-                int i = Integer.parseInt(value);
-                if(i < 1) throw new IllegalArgumentException();
-                return i;
-            }
-        }
-
+        out.println("Bound sphere(" + "block:" + block.block().getBlockId().name() + ",size:" + diameter + ") to brush.");
     }
 
 }
