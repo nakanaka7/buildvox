97e5f8c9482c06ff64540866055fd6ef27bfbad7
nakanaka7 <nakanaka7a@gmail.com>
2022-07-23 16:08:24 +0900

    changed scope of -m option to INHERIT

--- buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/mixin/Masked.java
+++ buildvox-core/src/main/java/tokyo/nakanaka/buildvox/core/command/mixin/Masked.java
@@ -3,7 +3,9 @@ package tokyo.nakanaka.buildvox.core.command.mixin;
 import picocli.CommandLine;
 
 public class Masked {
-    @CommandLine.Option(names = {"-m", "--masked"}, description = "Skips background block settings.")
+    @CommandLine.Option(names = {"-m", "--masked"},
+            description = "Skips background block settings.",
+            scope = CommandLine.ScopeType.INHERIT)
     private boolean masked;
 
     public boolean masked() {
